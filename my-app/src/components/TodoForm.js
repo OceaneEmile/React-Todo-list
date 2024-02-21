4760e378c69bb76f491663df246920f77c812b0d
OceaneEmile <oceane.emile@oclock.school>
2024-02-21 22:47:58 +0100

    Todo form

---
+++ my-app/src/components/TodoForm.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const TodoForm = () => {
+  return (
+    <form className='TodoForm'>
+        <input type='text' className='todo-input' placeholder='what is the task today?' />
+        <button className='todo-button'></button>
+    </form>
+  )
+}
