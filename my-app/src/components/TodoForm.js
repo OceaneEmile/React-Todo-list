4f1e56db7b6b202f18469c6b36e9681403aea4ec
OceaneEmile <oceane.emile@oclock.school>
2024-02-23 20:43:34 +0100

    array with this value on console log

--- my-app/src/components/TodoForm.js
+++ my-app/src/components/TodoForm.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React,{useState} from 'react'
 
-export const TodoForm = () => {
-  return (
-    <form className='TodoForm'>
-        <input type='text' className='todo-input' placeholder='what is the task today?' />
+export const TodoForm = ({addTodo}) => {
+  const [value,setValue] = useState("")
+  const handleSubmit = (e) =>{ 
+    e.preventDefault();
+    addTodo(value);
+  }
+  return ( 
+    <form className='TodoForm' onSubmit ={handleSubmit}>
+        <input type='text' className='todo-input' placeholder='what is the task today?'  onChange= {(e) =>setValue(e.target.value)}/>
         <button type='submit'className='todo-button'>Add Task</button>
     </form>
   )
